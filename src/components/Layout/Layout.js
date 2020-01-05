b0e0ae0ca7f092df9c460c2278d61bbcc82768ca
Simon Atkins <simonatk@gmail.com>
2020-01-05 11:45:44 +0000

    Add top Toolbar component with styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux'
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 export const Layout = props => (
     <Aux>
-        <div>Toolbar, Sidebar, Background</div>
+        <Toolbar />
         <main className={styles.Content}>{props.children}</main>
     </Aux>
 );
