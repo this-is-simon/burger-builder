771d0bc3a70f6f210e6a939321ba5e97fa9185cf
Simon Atkins <simon@flickapp.com>
2020-01-13 12:31:14 +0000

    Adjust dynamic styling for Logo in Sidedrawer vs Toolbar

--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -3,12 +3,14 @@ import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./Sidedrawer.module.css";
 
-const sideDrawer = (props) => {
+const sideDrawer = () => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
-                <NavigationItems />
+                <NavigationItems/>
             </nav>
         </div>
     )
