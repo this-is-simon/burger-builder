7a1d775d36efb605fb6a56ea7eee6c7074cb236e
Simon Atkins <simonatk@gmail.com>
2020-03-18 21:34:22 +0000

    Add componentWillUnmount on the axios interceptors in the ErrorHandler to avoid memory leaking

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,17 +6,24 @@ import Aux from '../Aux'
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
         state = {
-            error: null
+            error: null,
+            reqInterceptor: null,
+            resInterceptor: null
         }
 
         componentWillMount() {
-            axios.interceptors.request.use((req) => {
+            this.reqInterceptor = axios.interceptors.request.use((req) => {
                 this.setState({error: null})
                 return req
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error})
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
