6b382e6e858c8ff968136fcf8f97e6ba466c8641
Simon Atkins <simonatk@gmail.com>
2020-03-18 20:57:36 +0000

    Add logic to catch ingredient loading  error and return 'ingredients cant be loaded' message

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,13 +21,17 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     };
 
     componentDidMount() {
         axios.get('https://burger-builder-sa.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data})
+            })
+            .catch(error => {
+                this.setState({error: true})
             });
     }
 
@@ -112,7 +116,7 @@ class BurgerBuilder extends Component {
             disabledToggle[key] = disabledToggle[key] <= 0
         }
         let orderSummary = null;
-        let burger = <Spinner/>
+        let burger = this.state.error ? <p style={{textAlign: "center"}}>Ingredients can't be loaded...</p> : <Spinner/>
         if (this.state.ingredients) {
             burger = (
                 <Aux>
