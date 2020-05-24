51a6cb7c946425e7f245f1474d8eb97b8e07c1f3
Simon Atkins <simonatk@gmail.com>
2020-05-24 16:09:32 +0100

    Add Spinner when 'loading' on ContactData component and redirect to / route upon submit

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from "../../../components/UI/Button/Button";
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
+import Spinner from "../../../components/UI/Spinner/Spinner";
 
 class ContactData extends Component {
     state = {
@@ -35,17 +36,18 @@ class ContactData extends Component {
             .then(response => {
                 console.log(response);
                 this.setState({loading: false});
+                this.props.history.push("/");
             })
             .catch(error => {
                 console.log(error);
-                this.setState({loading: false});
+                this.setState({loading: false });
             })
     }
 
     render() {
-        return (
-            <div className={classes.ContactData}>
-                <h4>Enter Contact Details</h4>
+        let form;
+        if (!this.state.loading) {
+            form = (
                 <form>
                     <input type="text" name="name" placeholder="Your name" />
                     <input type="email" name="email" placeholder="Your email" />
@@ -53,6 +55,15 @@ class ContactData extends Component {
                     <input type="text" name="postcode" placeholder="Your postcode" />
                     <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
                 </form>
+            );
+        } else {
+            form = <Spinner />
+        }
+
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter Contact Details</h4>
+                {form}
             </div>
         )
     }
