a2c28df2e2b745b08efda71b17ee15f55038ef64
Simon Atkins <simonatk@gmail.com>
2020-05-28 00:12:12 +0100

    Pass ingredients through to Order and parse data for display

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,32 @@ import React from 'react'
 import classes from './Order.module.css';
 
 const Order = (props) => {
+    const ingredients = []
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return (
+            <span
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    padding: '5px'
+                }}
+                key={ig.name}>{ig.name} ({ig.amount})</span>
+        )
+    })
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>$5.45</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     )
 }
