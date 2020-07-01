429910daf100e988154a8346cdd3059b587c1f29
Simon Atkins <simon@flickapp.com>
2020-07-01 17:40:54 +0100

    Remove URL params for Checkout component and redux-ify it

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import {connect} from "react-redux";
 
 class BurgerBuilder extends Component {
     state = {
-        purchasable: false,
+        // purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -36,7 +36,8 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el
             }, 0);
-        this.setState({purchasable: sum > 0})
+        // this.setState({purchasable: sum > 0})
+        return sum > 0;
     };
 
     purchaseHandler = () => {
@@ -48,18 +49,7 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        const queryParams = [];
-
-        for (let i in this.props.ings) {
-            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.props.ings[i]))
-        };
-        queryParams.push("price=" + this.state.totalPrice);
-
-        const queryString = queryParams.join("&");
-        this.props.history.push({
-            pathname: '/checkout',
-            search: "?" + queryString
-        })
+        this.props.history.push('/checkout')
     };
 
     render() {
@@ -78,7 +68,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledToggle}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseableState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </Aux>
