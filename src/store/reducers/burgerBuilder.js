effdd9556e2eeb12ea6b8da773a4d4a881a4a906
Simon Atkins <simon@flickapp.com>
2020-08-25 17:11:16 +0100

    Add specificity to reducer so that ingredients appear in the burger in the same order they do in the + and - ingredients panel

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -36,7 +36,12 @@ const reducer = (state = initialState, action) => {
         case (actionTypes.SET_INGREDIENTS):
             return {
                 ...state,
-                ingredients: action.ingredients,
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    cheese: action.ingredients.cheese,
+                    meat: action.ingredients.meat,
+                    bacon: action.ingredients.bacon,
+                },
                 error: false
             }
         case (actionTypes.FETCH_INGREDIENTS_FAILED):
